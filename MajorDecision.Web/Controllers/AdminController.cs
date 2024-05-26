9393a95151add6d477c76695c7511a7328251ea9
Projkeen Theone <projkeen@outlook.com>
2024-05-26 13:06:13 +0300

    add first method for administration (delete questions without ApplicationUserId)

--- MajorDecision.Web/Controllers/AdminController.cs
+++ MajorDecision.Web/Controllers/AdminController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MajorDecision.Web.Data;
+using MajorDecision.Web.Data.Repositories.Abstract;
+using MajorDecision.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MajorDecision.Web.Controllers
@@ -6,9 +10,36 @@ namespace MajorDecision.Web.Controllers
     [Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        IWebHostEnvironment _hostingEnvironment;
+        private readonly IAuthenticationService _service;
+        private readonly ApplicationDbContext _db;
+
+        public AdminController(UserManager<ApplicationUser> userManager, IWebHostEnvironment hostingEnvironment, IAuthenticationService service, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _hostingEnvironment = hostingEnvironment;
+            _service = service;
+            _db = db;
+        }
         public IActionResult Display()
         {
-            return View();
+            var users = _db.Users.ToList();
+            return View(users.ToList());
         }
+
+        public async Task<IActionResult> ClearDataWithoutApplicationUserId()        
+        {
+            var decisionsWithoutApplicationUserId = _db.Decisions.Where(decisions => decisions.ApplicationUserId == null).ToList();
+            if (decisionsWithoutApplicationUserId.Count > 0)
+            {
+                _db.Decisions.RemoveRange(decisionsWithoutApplicationUserId);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Display", "Admin", TempData["msg"] = "data has been deleted");
+            }
+            
+            return RedirectToAction("Display", "Admin", TempData["msg"] = "No data");
+        }
+
     }
 }
